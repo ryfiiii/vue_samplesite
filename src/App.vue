94f28bec2a42852958714171e835e9dde1daa796
ryfiii <mzk114.cube@gmail.com>
2023-09-20 16:35:14 +0900

    ローディングアニメーション追加

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,33 @@
 <template>
-
-  <AppHeader />
-
-  <main>
-    <router-view v-slot="{ Component }">
-        <component :is="Component" />
-    </router-view>
-  </main>
-
-  <AppFooter />
+  <Loading v-show="loading"></Loading>
+  <div v-show="!loading">
+    <AppHeader />
+    <router-view></router-view>
+    <AppFooter />
+  </div>
 </template>
 
 <script>
 import AppHeader from "./components/AppHeader.vue"
 import AppFooter from "./components/AppFooter.vue"
+import Loading from "./components/Loading.vue"
 
 export default {
   name: 'App',
+  data() {
+    return {
+      loading: true,
+    }
+  },
+  mounted() {
+    setTimeout(() => {
+      this.loading = false
+    }, 3000)
+  },
   components: {
     AppHeader,
     AppFooter,
+    Loading,
   }
 }
 </script>
